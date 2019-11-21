42c814b1e8f1cd818aefd1084d8871e3f13ed1d1
guptayogansh <yogansh.11@gmail.com>
2019-11-22 02:07:11 +0530

    mongoose and bodyparser setup

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 let express = require("express")
+let bodyParser = require("body-parser")
+let mongoose = require("mongoose")
+let apiRoutes = require("./api-routes")
+
 let app = express()
-var port = process.env.port || 8081
-app.get('/', (req,res) => res.send("Hey! I am the Server for ApiHub and I\'m built using Express!!"))
-app.listen(port, function(){
+//configuring body parser to handle post requests
+app.use(bodyParser.urlencoded({
+    extended: true
+ }));
+app.use(bodyParser.json());
+
+//connect to mongoose
+let db = mongoose.connection;
+if(!db)
+    console.log("Error connecting db")
+else
+    console.log("Db connected successfully")
+
+let port = process.env.port || 8081
+app.use('/api', apiRoutes)
+app.get('/', (req,res) => res.send("Hey! I am the Server for ApiHub and I\'m built using Express!! Just installed nodemon for ease!"))
+app.listen(port, ()=>{
     console.log("Running ApiHub on port: " + port)
 })
