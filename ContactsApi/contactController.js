bd12ee83d8b06609dab972ae801ce5f3dac7a02b
guptayogansh <yogansh.11@gmail.com>
2019-11-22 02:23:21 +0530

    Model and Controller for Contacts

---
+++ ContactsApi/contactController.js
@@ -0,0 +1,95 @@
+//This controller will handle all the api requests.
+//It defined the method that handles request and response from different API endpoints
+// The following endpoint are implemented -
+// GET /api/contacts : list all contacts
+// POST /api/contacts : create new contact
+// GET /api/contacts/{id} : retrieve a single contact
+// PUT /api/contacts/{id} : update a single contact
+// DELETE /api/contacts/{id} : delete a single contact
+
+
+Contact = require('./contactModel');
+
+exports.index = function (req, res) {
+    Contact.get(function (err, contacts) {
+        if (err) {
+            res.json({
+                status: "error",
+                message: err,
+            });
+        }
+        res.json({
+            status: "success",
+            message: "Contacts retrieved successfully",
+            data: contacts
+        });
+    });
+};
+
+// Handle create contact actions
+exports.new = function (req, res) {
+    var contact = new Contact();
+    contact.name = req.body.name ? req.body.name : contact.name;
+    contact.gender = req.body.gender;
+    contact.email = req.body.email;
+    contact.phone = req.body.phone;
+
+// save the contact and check for errors
+    contact.save(function (err) {
+        // if (err)
+        //     res.json(err);
+res.json({
+            message: 'New contact created!',
+            data: contact
+        });
+    });
+};
+
+// Handle view contact info
+exports.view = function (req, res) {
+    Contact.findById(req.params.contact_id, function (err, contact) {
+        if (err)
+            res.send(err);
+        res.json({
+            message: 'Contact details loading..',
+            data: contact
+        });
+    });
+};
+
+// Handle update contact info
+exports.update = function (req, res) {
+Contact.findById(req.params.contact_id, function (err, contact) {
+        if (err)
+            res.send(err);
+contact.name = req.body.name ? req.body.name : contact.name;
+        contact.gender = req.body.gender;
+        contact.email = req.body.email;
+        contact.phone = req.body.phone;
+
+// save the contact and check for errors
+        contact.save(function (err) {
+            if (err)
+                res.json(err);
+            res.json({
+                message: 'Contact Info updated',
+                data: contact
+            });
+        });
+    });
+};
+
+
+// Handle delete contact
+exports.delete = function (req, res) {
+    Contact.remove({
+        _id: req.params.contact_id
+    }, function (err, contact) {
+        if (err)
+            res.send(err);
+res.json({
+            status: "success",
+            message: 'Contact deleted'
+        });
+    });
+};
